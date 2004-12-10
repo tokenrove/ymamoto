a3836c919461b1e2330bdceb072ad7ae5d9f831d
Julian Squires <tek@wiw.org>
2004-12-10 07:01:21 +0100

    changed to fixed frame durations, added arpeggios.

--- ymamoto.s
+++ ymamoto.s
@@ -8,27 +8,36 @@
 ;;; CONSTANT SYMBOLS
 
 number_of_channels = 3
-base_tempo = 140		; Beats Per Minute
+;;; This playback /was/ dependant on this value, but now all durations
+;;; are fixed cycles, so to change this, you should really just
+;;; recompile the songs with mumble.
 playback_frequency = 50		; Hertz
 
 	;; song data structure
-song_data_number_of_tracks = 0
-song_data_track_ptrs = 1
+song_data_arpeggio_pointer = 0
+song_data_venv_pointer = 4
+song_data_number_of_tracks = 8
+song_data_track_ptrs = 9
 
 	;; track data structure
 track_data_channel_ptrs = 0
-track_data_tempo = 4*number_of_channels
 
+	;; arpeggio table structure
+arpeggio_length = 0
+arpeggio_loop = 1
+arpeggio_data = 2
+	
 	;; song status structure
 song_current_track = 0
-song_current_tempo = 1
-song_whole_note_length = 3
-song_status_size = 5
+song_status_size = 1
 
 	;; channel status structure
 channel_counter = 0
 channel_data_ptr = 2
-channel_status_size = 6
+channel_arpeggio = 6
+channel_arp_position = 7
+channel_current_note = 8
+channel_status_size = 9
 
 ;;; FUNCTIONS
 
@@ -65,10 +74,6 @@ ymamoto_init:
 	MOVEA.L A0, A2
 	ADD.L song_data_track_ptrs(A0,D0), A2 ; ptr relative to song data.
 
-	;; set initial tempo
-	MOVE.B track_data_tempo(A2), D0
-	JSR set_tempo
-
 	;; setup each channel
 	LEA channel_status, A1
 	LEA.L track_data_channel_ptrs(A2), A2
@@ -78,16 +83,16 @@ ymamoto_init:
 	CLR channel_counter(A1)
 	MOVE.L (A2)+, channel_data_ptr(A1)
 	ADD.L D0, channel_data_ptr(A1) ; Make relative ptr absolute.
-	ADDQ #channel_status_size, A1
-	DBEQ D1, .reset_channel
+	MOVE.B #0, channel_arpeggio(A1)
+	ADD #channel_status_size, A1
+	DBF D1, .reset_channel
 
 .end:	
 	MOVEM.L (A7)+, D0-D1/A0-A2
 	RTS
 
 
-;;; ymamoto_update: call once per frame, a0 = song pointer.  Returns
-;;; d0 = $ffff if the song has finished, 0 otherwise.
+;;; ymamoto_update: call once per frame, a0 = song pointer.
 ymamoto_update:
 	MOVEQ #0, D0
 	JSR update_channel
@@ -101,7 +106,7 @@ ymamoto_update:
 ;;; update_channel: expects the song data pointer in A0, and the channel
 ;;; index in D0.
 update_channel:
-	MOVEM.L D0-D2/A0-A3, -(A7) ; save registers
+	MOVEM.L D1-D3/A0-A3, -(A7) ; save registers
 
 	MOVE.L #$FF8800, A3
 
@@ -111,7 +116,7 @@ update_channel:
 	BEQ .channel_status_loaded
 	SUBQ.B #1, D1
 .next_channel_status:
-	ADDQ #channel_status_size, A1
+	ADD #channel_status_size, A1
 	DBEQ D1, .next_channel_status
 .channel_status_loaded:
 
@@ -119,14 +124,62 @@ update_channel:
 	SUBQ.W #1, channel_counter(A1)
 	BPL .update_playing_note
 
-.load_new_command:
+
+;;; Command processing.
 	MOVEA.L channel_data_ptr(A1), A2
+.load_new_command:
 	MOVE.W (A2)+, D1
 	BPL .process_new_note
 	;; otherwise, this is a command.
-	;JSR handle_command
-	BRA .end
+	BTST #14, D1
+	BEQ .global_command
+
+
+	;; channel command
+	MOVE.W D1, D2
+	LSR.W #8, D2
+	AND.B #$3F, D2
+.arpeggio_command:
+	BNE .unknown_channel_command
+	;; if the arp value in D1 is 0, disable arpeggiation.
+	;; otherwise, set the arp bit in channel status.
+	MOVE.B D1, channel_arpeggio(A1)
+	MOVE.B #0, channel_arp_position(A1)
+
 	BRA .load_new_command
+
+.unknown_channel_command:
+	;; Just ignore it.
+	BRA .load_new_command
+
+
+.global_command:
+	MOVE.W D1, D2
+	LSR.W #8, D2
+	AND.B #$7F, D2
+	BNE .track_loop_command
+	;; this is a track end command ($8000).  Update the data pointer
+	;; to point to this command, and then end immediately.
+	SUBQ #2, A2
+	MOVE.L A2, channel_data_ptr(A1)
+	BRA .end
+
+.track_loop_command:
+	CMP #1, D2
+	BNE .trigger_command
+	;; XXX unimplemented
+	BRA .load_new_command
+
+.trigger_command:
+	CMP #2, D2
+	BNE .unknown_global_command
+	;; XXX unimplemented
+	BRA .load_new_command
+
+.unknown_global_command:
+	;; Just ignore it.
+	BRA .load_new_command
+
 
 .process_new_note:
 	MOVE.W D1, D2
@@ -134,6 +187,8 @@ update_channel:
 	CMPI.B #95, D2
 	BGT .special_note
 	;; otherwise, we need to start playing a tone.
+	MOVE.B D2, channel_current_note(A1)
+	MOVE.B #0, channel_arp_position(A1) ; reset arpeggio.
 	MOVEM.L D1/A1, -(A7)
 	LEA note_to_ymval_xlate, A1
 	LSL.B #1, D2
@@ -170,99 +225,98 @@ update_channel:
 	CMPI.B #126, D2		; Is it a wait (as opposed to a rest)?
 	BEQ .calculate_duration
 	;; Read current mixer setting and mute.
-	MOVEQ #7, D1
-	MOVE.B D1, (A3)
-	MOVE.B (A3), D1
-	BSET D0, D1
-	MOVE.B D1, 2(A3)
+	MOVEQ #7, D2
+	MOVE.B D2, (A3)
+	MOVE.B (A3), D2
+	BSET D0, D2
+	MOVE.B D2, 2(A3)
 
 .calculate_duration:
-	JSR calculate_duration
+	LSR.W #8, D1
 	MOVE.W D1, channel_counter(A1)
 
 .update_channel_data_ptr:
 	MOVE.L A2, channel_data_ptr(A1)
 
+
+;;; Effects processing.
+	;; The note value is loaded from channel status, and effects
+	;; which have an effect at a chromatic tone level are
+	;; processed, first (arpeggios).  Then the frequency is looked
+	;; up, and effects which change the raw frequency (portamento)
+	;; are processed.  Finally any other effects (venv, hard
+	;; envelope) are processed.
 .update_playing_note:
-	;; update effects
+	MOVE.B channel_current_note(A1), D3
 
+	;; Note value effects.
+
+.update_arpeggio:		; Arpeggios.
+	MOVE.B channel_arpeggio(A1), D1
+	BEQ .lookup_frequency
+	MOVE.L song_data_arpeggio_pointer(A0), A2
+	MOVE A0, D2
+	ADD.L D2, A2
+	CLR.L D2
+	ADDQ #1, A2		; skip length of table.
+	;; find our entry in the table.
+	SUBQ #1, D1
+	BEQ .arp_lookup_finished
+	SUBQ #1, D1
+.next_arp_entry:
+	MOVE.B (A2), D2
+	ADD #2, D2
+	ADD D2, A2
+	DBF D1, .next_arp_entry
+.arp_lookup_finished:
+	MOVE.B channel_arp_position(A1), D2
+	;; load length; if arp position greater than length,
+	;; reset to loop point.
+	MOVE.B arpeggio_length(A2), D1
+	CMP.B D2, D1
+	BGT .arp_update_note
+	MOVE.B arpeggio_loop(A2), D2
+.arp_update_note:
+	;; load arp delta, update playing note.
+	MOVE.B arpeggio_data(A2,D2), D1
+	ADD.B D1, D3
+	MOVE.B D3, channel_current_note(A1)
+	;; update arp position.
+	ADDQ #1, D2
+	MOVE.B D2, channel_arp_position(A1)
+
+	;; envelope follow mode would go here
+.lookup_frequency:
+	LEA note_to_ymval_xlate, A2
+	LSL.B #1, D3
+	MOVE.W (A2,D3), D3
+
+	;; Frequency value effects.
+
+.set_frequency:
+	MOVEQ #0, D1
+	ADD.B D0, D1
+	ADD.B D0, D1
+	MOVE.B D1, (A3)
+	MOVE.B D3, 2(A3)
+
+	LSR.L #8, D3
+	MOVEQ #1, D1
+	ADD.B D0, D1
+	ADD.B D0, D1
+	MOVE.B D1, (A3)
+	MOVE.B D3, 2(A3)
+
+	;; Other effects.
+.other_effects:	
+.update_venv:			; Soft volume envelope.
+
+
+;;; End of main playroutine.
 .end:	
-	MOVEM.L (A7)+, D0-D2/A0-A3 ; restore registers
+	MOVEM.L (A7)+, D1-D3/A0-A3 ; restore registers
 	RTS
-
-
-;;; handle_command
-;;; D0 = channel number, D1 = command;
-;;; A0 = song pointer, A1 = channel status, A2 = channel data stream.
-handle_command:
-	RTS
-
-
-;;; calculate_duration
-;;; D1 = unprocessed note data.
-;;; Returns duration in D1.
-calculate_duration:
-	MOVEM.L D0/D2/A0, -(A7)
-
-	LSR.W #8, D1		; Cut off tone info.
-	MOVE.B D1, D0
-	AND.B #7, D0
-	LEA song_status, A0
-	MOVE.W song_whole_note_length(A0), D2
-	LSR.W D0, D2		; Divide whole note length.
-
-.test_dot:			; This unused label is for clarity.
-	BTST #3, D1
-	BEQ .test_double_dot
-	MOVE.W D2, D0
-	LSR.W #1, D0
-	ADD.W D0, D2
-
-.test_double_dot:
-	BTST #4, D1
-	BEQ .test_triplet
-	MOVE.W D2, D0
-	LSR.W #1, D0
-	ADD.W D0, D2
-	LSR.W #1, D0
-	ADD.W D0, D2
-
-.test_triplet:
-	BTST #5, D1
-	BEQ .test_tie
-	;;; XXX multiply by 2/3
-	BRA .test_tie
-	;; The following code is for duplets (3/2).
-	MOVE.W D2, D0		; D2 = D2 * 3/2
-	ADD.W D0, D2
-	ADD.W D0, D2
-	LSR.W #1, D2
-
-.test_tie:
-	BTST #6, D1
-	BEQ .end
-	;; XXX set special flag.
-
-.end:	
-	MOVE.W D2, D1		; Return duration.
-	MOVEM.L (A7)+, D0/D2/A0
-	RTS
-
-
-;;; set_tempo
-;;; D0 = (unprocessed) tempo value;
-;;; A1 = song status pointer.
-;;; Wipes out D0 and D1.
-set_tempo:
-	EXT.W D0
-	ADDI.W #base_tempo, D0
-	MOVE.W D0, song_current_tempo(A1)
-	;; FIXME could be cool to not have to recompile to change this.
-	MOVE.W #playback_frequency*60, D1 ; D1 = cycles per minute.
-	LSR.W #2, D0		; D0 = BPM/4 = whole notes per minute.
-	DIVU D0, D1		; D1 (low word) = ticks per whole note.
-	MOVE.W D1, song_whole_note_length(A1)
-	RTS
+;;;
 
 
 ;;; CONSTANT TABLES
@@ -270,7 +324,7 @@ set_tempo:
 note_to_ymval_xlate:
 	DC.W $EEE,$E18,$D4D,$C8E,$BDA,$B2F,$A8F,$9F7,$968,$8E1,$861,$7E9
 	DC.W $777,$70C,$6A7,$647,$5ED,$598,$547,$4FC,$4B4,$470,$431,$3F4
-	DC.W $3BC,$386,$353,$324,$2F6,$2CC,$2A4,$27E,$25A,$328,$218,$1FA
+	DC.W $3BC,$386,$353,$324,$2F6,$2CC,$2A4,$27E,$25A,$238,$218,$1FA
 	DC.W $1DE,$1C3,$1AA,$192,$17B,$166,$152,$13F,$12D,$11C,$10C,$FD
 	DC.W $EF,$E1,$D5,$C9,$BE,$B3,$A9,$9F,$96,$8E,$86,$7F
 	DC.W $77,$71,$6A,$64,$5F,$59,$54,$50,$48,$47,$43,$3F
